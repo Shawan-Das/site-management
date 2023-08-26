3f4c5f04aff7761d6bec331f337c58ef6fa1c126
Shawan Das <86365217+Shawan-Das@users.noreply.github.com>
2023-08-27 00:17:59 +0600

    New Employee Management System

---
+++ DotNerCore6/EmployeeManagement/EmployeeManagement/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+namespace EmployeeManagement.Models
+{
+	[Table("department")]
+	public class Department
+	{
+		[Key]
+		[Column("department_id")]
+		public string? department_id { get; set; }
+		[Column("dep[artment_name")]
+		public string? department_name { get; set; }
+	}
+}
---
+++ DotNerCore6/EmployeeManagement/EmployeeManagement/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManagement.Models
+{
+	[Table ("users")]
+	public class User
+	{
+		[Key]
+		[Column("user_name")]
+		public string? user_name { get; set; }
+		[Column("email")]
+		public string? email { get; set; }
+		[Column("password")]
+		public string? password { get; set; }
+	}
+}
