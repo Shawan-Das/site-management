6897e27e3c561426e30629f6f465771f28af2078
Shawan Das <86365217+Shawan-Das@users.noreply.github.com>
2025-11-12 11:58:03 +0600

    folder cleanup

--- HotelBookingAPI/Models/HotelBooking.cs
+++
@@ -1,9 +0,0 @@
-namespace HotelBookingAPI.Models
-{
-    public class HotelBooking
-    {
-        public int Id { get; set; }
-        public int RoomNumber { get; set; }
-        public string? ClientName { get; set; }
-    }
-}
